ee78b0b2cc4c18e061265d22c435ce7c09ddfff1
Ivaylo Angelov <82383097+ivaylo-angelov@users.noreply.github.com>
2024-07-22 04:27:52 +0300

    Use /populate path name and POST method

--- main.tf
+++ main.tf
@@ -104,13 +104,13 @@ resource "aws_api_gateway_rest_api" "random_data_api" {
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.random_data_api.id
   parent_id   = aws_api_gateway_rest_api.random_data_api.root_resource_id
-  path_part   = "data"
+  path_part   = "populate"
 }
 
 resource "aws_api_gateway_method" "api_method" {
   rest_api_id   = aws_api_gateway_rest_api.random_data_api.id
   resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
